58467b1f565851dce8f431b3988d6516c49ca1a1
mahourpriyank <mahourpriyank40@gmail.com>
2018-09-06 23:07:39 +0530

    1.created model    2.registered model     3.created superuser

--- formsproject/basic_app/admin.py
+++ formsproject/basic_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from basic_app.models import Student
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id','name','marks']
+
+admin.site.register(Student,StudentAdmin)
