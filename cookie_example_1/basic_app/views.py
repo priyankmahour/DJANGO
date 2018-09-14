0d07f139276a7b0a4e23c7f648b22357d65a4048
mahourpriyank <mahourpriyank40@gmail.com>
2018-09-15 01:04:06 +0530

    1. A cookie that counts no of requests made

---
+++ cookie_example_1/basic_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def count_view(request):
+    count = int(request.COOKIES.get('count',0))
+    new_count = count+1
+    context = {'count':new_count}
+    response = render(request,"basic_app/count.html",context)
+    response.set_cookie('count',new_count)
+    return response
